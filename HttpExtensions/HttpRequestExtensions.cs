51e9b420b7477cc9efdb104cf2557f2afa8dca3a
Ivan Van Heerden <ivan.van_heerden@bmwgroup.com>
2019-08-06 15:56:38 +0200

    Initial commit

---
+++ HttpExtensions/HttpRequestExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace HttpExtensions
+{
+    public static class HttpRequestExtensions
+    {
+        public static T GetQueryString<T>(this HttpRequest request, string key, T defaultValue)
+        {
+            if (request.Query is null)
+                return defaultValue;
+
+            var match = request.Query.FirstOrDefault(kv => String.Compare(kv.Key, key, StringComparison.OrdinalIgnoreCase) == 0);
+            if (string.IsNullOrWhiteSpace(match.Value))
+                return defaultValue;
+
+            try
+            {
+                var typeConverter = TypeDescriptor.GetConverter(typeof(T));
+                return (T)typeConverter.ConvertFromString(match.Value);
+            }
+            catch (Exception)
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
