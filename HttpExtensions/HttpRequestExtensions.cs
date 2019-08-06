26fc56af9917cbe3b5b26e92bddf3bc19eaf4169
Ivan Van Heerden <ivan.van_heerden@bmwgroup.com>
2019-08-06 16:07:18 +0200

    Fix namespace

--- HttpExtensions/HttpRequestExtensions.cs
+++ HttpExtensions/HttpRequestExtensions.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
 
-namespace HttpExtensions
+namespace Uhuru.Extensions
 {
     public static class HttpRequestExtensions
     {
